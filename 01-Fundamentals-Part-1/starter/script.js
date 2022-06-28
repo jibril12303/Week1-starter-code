ec5512a5a1d4fd1ad3cdfe22f0c6832aa8498e98
jibril yusuf <jibril_2012@live.com>
2022-06-28 19:30:29 +0100

    udemy variables work completed

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,34 @@
-
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is fun!');
+console.log(40+22+333) // values
+
+console.log ('Jonas'); // Value 
+console.log(23); 
+// Variable is a name of memory location. There are three types of variables in java: local, instance and static.
+
+let firstName = "jibril";
+
+let salah = "today";
+
+let myFirstJob = 'Progammers';
+let myCurrentJob = 'Teacher'
+console.log(myCurrentJob);
 
 
-console.log(40+22+333)
+let mohamed = 'salah' ;
+console.log(mohamed)
+
+let football1 = 'coders' ;
+console.log(football1)
+
+let myFirstTechJob = 'technolgy'; 
+console.log(myFirstTechJob)
+
+
+
+let myCountry = 'somalia';
+let myContinent = 'africa';
+let myCountryPopulation = '10million';
+
+console.log(myCountry)
+console.log(myContinent)
+console.log(myCountryPopulation)
