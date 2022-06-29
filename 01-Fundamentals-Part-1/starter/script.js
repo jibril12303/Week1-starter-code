72c80baf017c7656ae87425ebe12277e792e98c3
jibril yusuf <jibril_2012@live.com>
2022-06-29 17:34:32 +0100

    different data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40+22+333) // values
 
@@ -32,3 +33,44 @@ let myCountryPopulation = '10million';
 console.log(myCountry)
 console.log(myContinent)
 console.log(myCountryPopulation)
+
+
+
+var life = 100;
+
+life = life - 1000777
+console.log(life)
+
+var name = "Dev ED";
+
+var checkout = false;
+
+var box;  
+
+let javascriptISFUN = true; 
+console.log(javascriptISFUN);
+
+const jibril = 23
+
+console.log(typeof false);
+console.log(typeof javascriptISFUN);
+console.log(typeof 88);
+console.log('jibril');
+
+*/
+let javascriptISFUN = true;
+console.log(javascriptISFUN);
+
+console.log(typeof javascriptISFUN);
+
+javascriptISFUN = 'True!';
+console.log(typeof javascriptISFUN)
+
+
+let month;
+console.log(month);
+console.log(typeof month)
+
+month = 2000
+console.log(month);
+
