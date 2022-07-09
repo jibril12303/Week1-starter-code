8e3dc84ecb24a80d18d13090fe0451f2215aad6a
jibril yusuf <jibril_2012@live.com>
2022-07-09 18:57:50 +0100

    variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -88,7 +88,7 @@ let job = 'programmer';
 job = 'Coder!';
 console.log(job)
 
-*/
+
 // Basic operators
 const now = 2022
 const ageJibril = now - 1999;
@@ -121,5 +121,181 @@ console.log(BMI2 / (markHeight * markHeight))
 const markHigherBMI = BMI1 > BMI2
 console.log(markHigherBMI)
 
-/*BMI = mass / height ** 2 = mass / (height * height) (mass in kg
-and height in meter) */
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter)
+
+
+const firstName = "jibril";
+const job = "teacher";
+const birthYear = 1999;
+const actualYear = 2022;
+
+const jibril =
+  "i'm " +
+  firstName +
+  " a " +
+  (actualYear - birthYear) +
+  " years old " +
+  job +
+  "! ";
+console.log(jibril);
+
+const jibrilNew = `i'm ${firstName} a ${
+  actualYear - birthYear
+} year old ${job}!!!`;
+console.log(jibrilNew);
+
+console.log(`im playing about......`);
+// new line \\n
+console.log(`string with a \n\
+wait for me \n\ `);
+
+// object {}
+// boolean true, false
+// string '', ""
+// template ``
+
+const writingMessage = `this a beautiful \n\and we are working hard to make it out of the hood`;
+
+const stregnth = " togethter we great and lovely \n\
+we know whats up";
+console.log(stregnth);
+
+let msg = ["This text", "can", "span multiple lines"].join("\n");
+
+console.log(msg);
+ 
+
+
+
+const time = 1600;
+
+if( time <= 1500) {
+  console.log('the timing that you begin driving is soon')
+} else {
+  const timeLeft = 1800 - time;
+  console.log(`theres a few minuetes left. Wait another ${timeLeft} minuetes `)
+}
+
+const age = 22;
+
+
+if(age <= 25) {
+  console.log('Jibril can begin to drive')
+} else {
+  const isOldEnough = 25 < 29
+  console.log( `Jibril is to young to drive ${isOldEnough}`)
+}
+
+const birthYear = 1991;
+
+
+let cent
+if (birthYear <= 2000) {
+  cent = 20;
+} else {
+  cent = 21;
+}
+console.log(cent);
+
+
+if (condition) {
+  statement 
+}
+else (another)
+
+
+
+let age = 18;
+let state = 'CA';
+
+if (state == 'CA') {
+  if (age >= 18) {
+    console.log('You can drive.');
+  } else {
+    const isOldEnough = age < 222
+    console.log(`Then you are too young to drive `)
+  }
+}
+
+
+var personName = 'BoB';
+var pets = 2;
+var funFact = 'i am from mars';
+
+console.log(`My name is ${personName}, i have ${pets} and ${funFact} `)
+
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+
+
+const markWeight = 95;
+const markHeight = 1.88;
+const johnWeight = 85;
+const johnHeight = 1.76;
+
+
+const BMIMark = markWeight / markHeight ** 2
+const BMIJohn = johnWeight / (johnHeight * 2);
+console.log(BMIJohn, BMIMark);
+
+
+if (BMIMark > BMIJohn) {
+  console.log(`Marks BMI (${BMIMark}is higher than Johns (${BMIJohn})`)
+} else {
+  console.log(`Johns BMI (${BMIJohn}) is higher than Marks (${BMIMark})`)
+}
+
+
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear );
+console.log(inputYear + 18);
+
+
+var myName = "Bou";
+
+myName = 8;
+
+const ourNmae = "freecodecamp";
+
+const pi = 3.14;
+
+
+a = 7;
+console.log(a)
+
+
+
+//intilialize these three variables
+
+var a = 5;
+var b = 10;
+var c = "i am a string";
+
+console.log(a, b, c)
+
+
+*/
+
+// Declare variables using var
+var personName = 'Sakura';
+var pets = 3;
+var funFact = 'I like pineapple on my pizza.';
+
+// Use + to combine data and variable names in a single console log
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact);
+
+// When re-assigning variables, use variable name
+personName = 'Mateo';
+pets = 5;
+funFact = 'I was a yo-yo champ in third grade.';
+
+// Logs message with re-assigned values
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact);
